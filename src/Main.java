370e54e3885d826cf713f58df3a9976e57754ddf
Salar86 <99397531+Salar86@users.noreply.github.com>
2023-02-20 10:29:53 +0100

    add getInt

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println(getString("hvad hedder du?"));
+        System.out.println(getInt("Angiv venligst din skostørrelse"));
 
     }
     public static String getString(String s) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(s+" : ");
         return scanner.nextLine();
+    }
+    public static int getInt(String s) {
+        int res=0;
+
+        while (true) {
+            try {
+            res = Integer.parseInt(getString(s));
+            return res;
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Husk ikke talord");
+            }
+        }
     }
 }
