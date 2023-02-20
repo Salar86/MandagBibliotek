c1f3e179ba94426fc4208153015e75d526d013cc
Salar86 <99397531+Salar86@users.noreply.github.com>
2023-02-20 10:12:01 +0100

    laver getString

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(getString("hvad hedder du?"));
 
     }
-    public String getInput(String s) {
+    public static String getString(String s) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(s+" : ");
         return scanner.nextLine();
