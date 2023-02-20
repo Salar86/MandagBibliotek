82ed70f8b62e8bd15042f544deaf34349c260265
Salar86 <99397531+Salar86@users.noreply.github.com>
2023-02-20 09:40:03 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public String getInput(String s) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(s+" : ");
+        return scanner.nextLine();
+    }
+}
